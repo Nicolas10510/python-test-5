7246316c2f61b431137b04cd00e7d1c3c6365668
Nikolay Gurov <101382231+Nicolas10510@users.noreply.github.com>
2024-07-22 14:06:53 +0300

    add adjustments

--- module_testing.py
+++ module_testing.py
@@ -1,6 +1,24 @@
 import unittest
+from solution import *
+
 
 # Place your tests here
+
+class Test_Programmer(unittest.TestCase):
+
+  def setUp(self):
+    self.programmer = Programmer('Ivan', Positions.JUNIOR.value)
+
+
+  def test_work(self):
+    self.assertEqual(self.programmer.work(10), None)
+
+  def test_rise(self):
+    self.assertEqual(self.programmer.rise(), None)
+
+  def test___give_salary(self):
+    self.assertEqual(self.programmer.__give_salary(), None)
+
 
 if __name__ == '__main__':
   unittest.main()
--- solution.py
+++ solution.py
@@ -1,32 +1,58 @@
-class Positions(Enum):  
+from __future__ import annotations
+from enum import Enum
+
+
+class Positions(Enum):
     JUNIOR = 10  
-    MIDDLE = 12  
+    MIDDLE = 15
     SENIOR = 20  
   
   
-class Programmer:  
-    def __init__(self, name: float, position: Positions) -> None:  
-        self.__name = name  
-        self.__position = position  
-        self.__hour_price = self.__position.value  
-  
-    def work(self, time: int) -> None:  
-        self.__time += time  
-  
-    def rise(self) -> str:  
-        if self.__position.name == 'JUNIOR':  
-            self.__position = Positions.MIDDLE  
-  
-        elif self.__position.name == 'MIDDLE':  
-            self.__position = Positions.SENIOR  
-  
-        else:  
-            self.__hour_price += 2 
-  
-    def info(self) -> str:  
-        return f'{self.__name}: {self.__time} ч. {self.__give_salary()} тгр.'  
-  
-    def __give_salary(self) -> int:  
-        salary = self.__hour_price // self.__time  
-        self.__time = 0  
+class Programmer:
+    def __init__(self, name: str, position: Positions, time: int = 0) -> None:
+        self.__name = name
+        self.__position = position
+        self.__time = time
+        self.__hour_price = self.__position
+
+    def work(self, time: int) -> None:
+        self.__time += time
+
+    def rise(self) -> None:
+        if self.__position == Positions.JUNIOR.value:
+            self.__position = Positions.MIDDLE.value
+            self.__hour_price = Positions.MIDDLE.value
+
+        elif self.__position == Positions.MIDDLE.value:
+            self.__position = Positions.SENIOR.value
+            self.__hour_price = Positions.SENIOR.value
+
+        else:
+            self.__hour_price += 1
+
+    def info(self) -> str:
+        return f'{self.__name}: {self.__time} ч. {self.__give_salary()} тгр.'
+
+    def __give_salary(self) -> int:
+        salary = self.__hour_price * self.__time
+        self.__time = 0
         return salary
+
+
+if __name__ == '__main__':
+    programmer_0 = Programmer('Ivan', Positions.JUNIOR.value)
+    programmer_0.work(10)
+    print(programmer_0.info())
+
+    programmer_0.rise()
+    programmer_0.work(10)
+    print(programmer_0.info())
+
+    programmer_0.rise()
+    programmer_0.work(10)
+    print(programmer_0.info())
+
+    programmer_0.rise()
+    programmer_0.work(10)
+    print(programmer_0.info())
+
